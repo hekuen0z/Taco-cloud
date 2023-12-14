fa6f710135c8dda41239455be28079510f36fa29
hekuen0z <kaptur.swdev@gmail.com>
2023-12-14 09:06:49 +0300

    create project and add model "Taco"

---
+++ src/main/java/vesennyaya/models/Taco.java
@@ -0,0 +1,9 @@
+package vesennyaya.models;
+
+import java.util.List;
+
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
---
+++ src/test/java/vesennyaya/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package vesennyaya;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import vesennyaya.controllers.HomeController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homePage"))
+                .andExpect(content().string(
+                        Matchers.containsString("Welcome")
+                ));
+    }
+}
