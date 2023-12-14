e806da14c16ec0cb3a6b21c1fc25919a4021b93c
hekuen0z <kaptur.swdev@gmail.com>
2023-12-14 12:46:19 +0300

    add tests for designTacoController

---
+++ src/test/java/edu/taco_cloud/DesignTacoControllerTest.java
@@ -0,0 +1,46 @@
+package edu.taco_cloud;
+
+import edu.taco_cloud.controllers.DesignTacoController;
+import edu.taco_cloud.models.Taco;
+import edu.taco_cloud.models.TacoOrder;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DesignTacoController.class)
+class DesignTacoControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testDesignPage() throws Exception {
+        mockMvc.perform(get("/design"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("designPage"))
+                .andExpect(content().string(
+                        containsString("Design your taco!")
+                ));
+
+    }
+
+    @Test
+    void testTacoAddedToModelAttribute() throws Exception {
+        mockMvc.perform(get("/design"))
+                .andExpect(model().attribute("taco", equalTo(new Taco())))
+                .andExpect(model().attribute("tacoOrder", equalTo(new TacoOrder())))
+                .andExpect(model().attribute("wrap", hasSize(2)))
+                .andExpect(model().attribute("protein", hasSize(2)))
+                .andExpect(model().attribute("sauce", hasSize(2)));
+    }
+}
